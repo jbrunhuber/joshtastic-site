10d1245b6df11441cb1065749cedd4b68a5d5277
Joshua Brunhuber <joshua.brunhuber@empiriecom.com>
2017-12-01 10:46:35 +0100

    just a little test

---
+++ testapp/main.js
@@ -0,0 +1,8 @@
+
+$(function() {
+    alert("Josh's site loaded successfully" );
+    window.location.href = "https://apple.com";
+    alert("APPLE!!");
+    window.location.href = "https://google.com";
+    alert("GOOGLE!");
+});
